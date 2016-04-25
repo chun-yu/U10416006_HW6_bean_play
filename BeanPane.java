4a48bdde1e327fc1e43af5aea2ebd96ec5b1044e
chun-yu <rmp4m3jun@gmail.com>
2016-04-25 18:41:32 +0800

    change another way to type the code
    decrease the lines and simplify the code

--- BeanPane.java
+++ BeanPane.java
@@ -26,183 +26,76 @@ public class BeanPane extends Application{
 		primaryStage.show();
 	}
 }
-
 class  LinePane extends Pane {
-		final double radius = 11;
+		final double radius = 9;
 	double x = 350, y = 40;
 	double dx = 1, dy = 1;
 	Circle circle00 = new Circle(x, y, radius);
 	Timeline animation;
 	public LinePane(){
-		
-
-
-	
 		circle00.setFill(Color.GREEN); // Set ball color
-
-
 		// Create an animation for moving the ball
 		animation = new Timeline(new KeyFrame(Duration.millis(8), e -> moveBall()));
 		animation.setCycleCount(Timeline.INDEFINITE);
 		animation.play(); // Start animation
-	
 
-
-		
 		Polygon polygon = new Polygon();
-		polygon.getPoints().addAll(new Double[]{
-			320.0, 30.0,
-		    320.0, 100.0,
-		    150.0, 520.0,
-		    150.0, 600.0,
-		    600.0, 600.0,
-		    600.0, 520.0,
-		    420.0, 100.0,
-		    420.0, 30.0});
+		polygon.getPoints().addAll(new Double[]{320.0, 20.0   ,320.0, 100.0   ,150.0,520.0    ,150.0,600.0    ,600.0, 600.0   ,600.0,520.0    ,420.0,100.0   ,420.0,20.0});
 		polygon.setStrokeWidth(6);
 		polygon.setStroke(Color.GRAY);
 		polygon.setFill(null);
-		Line line1 = new Line(200,530,200,600);
-		Line line2 = new Line(250,530,250,600);
-		Line line3 = new Line(300,530,300,600);
-		Line line4 = new Line(350,530,350,600);
-		Line line5 = new Line(400,530,400,600);
-		Line line6 = new Line(450,530,450,600);
-		Line line7 = new Line(500,530,500,600);
-		Line line8 = new Line(550,530,550,600);
 
-		Circle circle2 = new Circle(370,90,10,Color.PURPLE);
-		Circle circle3 = new Circle(345,135,10,Color.PURPLE);
-		Circle circle4 = new Circle(395,135,10,Color.PURPLE);
-		Circle circle5 = new Circle(320,200,10,Color.PURPLE);
-		Circle circle6 = new Circle(370,200,10,Color.PURPLE);
-		Circle circle7 = new Circle(420,200,10,Color.PURPLE);
-		Circle circle8 = new Circle(295,265,10,Color.PURPLE);
-		Circle circle9 = new Circle(345,265,10,Color.PURPLE);
-		Circle circle10 = new Circle(395,265,10,Color.PURPLE);
-		Circle circle11 = new Circle(445,265,10,Color.PURPLE);
-		Circle circle12 = new Circle(270,330,10,Color.PURPLE);
-		Circle circle13 = new Circle(320,330,10,Color.PURPLE);
-		Circle circle14 = new Circle(370,330,10,Color.PURPLE);
-		Circle circle15 = new Circle(420,330,10,Color.PURPLE);
-		Circle circle16 = new Circle(470,330,10,Color.PURPLE);
-		Circle circle17 = new Circle(245,395,10,Color.PURPLE);
-		Circle circle18 = new Circle(295,395,10,Color.PURPLE);
-		Circle circle19 = new Circle(345,395,10,Color.PURPLE);
-		Circle circle20 = new Circle(395,395,10,Color.PURPLE);
-		Circle circle21 = new Circle(445,395,10,Color.PURPLE);
-		Circle circle22 = new Circle(495,395,10,Color.PURPLE);
-		Circle circle23 = new Circle(220,460,10,Color.PURPLE);
-		Circle circle25 = new Circle(270,460,10,Color.PURPLE);
-		Circle circle26 = new Circle(320,460,10,Color.PURPLE);
-		Circle circle27 = new Circle(370,460,10,Color.PURPLE);
-		Circle circle28 = new Circle(420,460,10,Color.PURPLE);
-		Circle circle29 = new Circle(470,460,10,Color.PURPLE);
-		Circle circle30 = new Circle(520,460,10,Color.PURPLE);
-		Circle circle31 = new Circle(200,530,10,Color.RED);
-		Circle circle32 = new Circle(250,530,10,Color.RED);
-		Circle circle33 = new Circle(300,530,10,Color.RED);
-		Circle circle34 = new Circle(350,530,10,Color.RED);
-		Circle circle35 = new Circle(400,530,10,Color.RED);
-		Circle circle36 = new Circle(450,530,10,Color.RED);
-		Circle circle37 = new Circle(500,530,10,Color.RED);
-		Circle circle38 = new Circle(550,530,10,Color.RED);
-
-		
-		line1.setStrokeWidth(5);
-		line2.setStrokeWidth(5);
-		line3.setStrokeWidth(5);
-		line4.setStrokeWidth(5);
-		line5.setStrokeWidth(5);
-		line6.setStrokeWidth(5);
-		line7.setStrokeWidth(5);
-		line8.setStrokeWidth(5);
-		
-		line1.setStroke(Color.BLACK);
-		getChildren().addAll(circle00,polygon,line1,line2,line3,line4,line5,line6,line7,line8,circle2,circle3,circle4,circle5,circle6,circle7,circle8,circle9,circle10,circle11,circle12,circle13,circle14,circle15,circle16,circle17,circle18,circle19,circle20,circle21,circle22,circle23,circle25,circle26,circle27,circle28,circle29,circle30,circle31,circle32,circle33,circle34,circle35,circle36,circle37,circle38);
-		
+		int[] linex ={200,250,300,350,400,450,500,550}; 
+		int[] cirx = {370,345,395,320,370,420,295,345,395,445,270,320,370,420,470,245,295,345,395,445,495,220,270,320,370,420,470,520};
+		int q=0;
+		for (int k = 0; k < 7 ; k++){		//add middle circles
+			for (int i = 0 ; i <= k ; i++){
+				q= q + 1;
+				int x = cirx[q-1];
+				Circle circle = new Circle(x,70+65*k,10,Color.PURPLE);
+				getChildren().add(circle);
+			}
+		}
+		for (int i =0 ; i <=7;i++){			//add  line1,line2,line3,line4,line5,line6,line7,line8
+			int x = linex[i];
+			Line line = new Line(x,530,x,600);
+			line.setStrokeWidth(5);
+			line.setStroke(Color.BLACK);
+			getChildren().add(line);
+		}
+		for (int i = 0 ; i < 8 ; i++){		//add circles at most lower
+			Circle circle = new Circle(200 + 50*i,530,10,Color.RED);
+			getChildren().add(circle);
+		}
+		getChildren().addAll(circle00,polygon);
 }
     protected void moveBall(){
     int random;
-    double d = 0.6;
+    double d = 1;
     
     if(y >= 40 && y < 590){
-        if (y == 70) {
-            random = (int)(Math.random() * 2);
-            if(random == 1){
-                dx *= -d; 	
-            }else{
-                dx *= d; 
-            }
-		 
-        }
-        if(y == 115){
-            random = (int)(Math.random() * 2);            
-            if(random == 1){
-                if(dx == d||dx == -d){
-                    dx *= -1; 	
-                }
-                else dx *= -d;
-            }else{
-                if(dx == d||dx == -d){
-                    dx *= 1; 
-                }
-                else dx *= d;
-            }
-        }
-        if(y == 180){
-            random = (int)(Math.random() * 2);
-            if(random == 1){
-                dx *= -1; 	
-            }else{
-                dx *= 1; 
-            }
-        }
-        if(y == 310){
-            random = (int)(Math.random() * 2);
-            if(random == 1){
-                dx *= -1; 	
-            }else{
-                dx *= 1; 
-            }
-        }
-        if(y == 375){
-            random = (int)(Math.random() * 2);
-            if(random == 1){
-                dx *= -1; 	
-            }else{
-                dx *= 1; 
-            }
-        }
-        if(y == 440){
-            random = (int)(Math.random() * 2);
-            if(random == 1){
-                dx *= -1; 	
-            }else{
-                dx *= 1; 
-            }
-	}
-        if(y == 510){
-            random = (int)(Math.random() * 2);
-            if(random == 1){
-                dx *= -1; 	
-            }else{
-                dx *= 1; 
-            }
-	}
-	        if(y == 530){
-            dx *= -1;
-			
-	}
-	        if(y == 550){
-            dx *= -1;
-	}
-	
+		for (int i = 0 ; i <= 7 ;i++){
+			if (y == 70 + 65*i ) {
+					random = (int)(Math.random() * 2);
+					if(random == 1){
+					dx *= -d; 	
+					}else{
+					dx *= d; 
+					}
+				
+			}
+			if (x <= 310-27.5*i || x >= 420 + 27.5*i ){
+				dx *= -1; }
+		}
+		for(int i = 0 ; i < 10 ; i++){
+		if(x== 150 + 50*i){
+			dx *= -1;
+		}}
         x += dx;
-    }
-    if(y == 585){
+		if(y == 585){
         dy *= 0;
 		dx *= 0; 
+		}
     }
     // Adjust ball position
     y += dy;
